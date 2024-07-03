12cf67fc8a557d2551cc10e3a4f5b8b821660c84
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-07-03 21:43:46 +0300

    add cloudinary server to upload Profile Images

--- back/index.js
+++ back/index.js
@@ -7,6 +7,7 @@ const userRouter = require("./routes/user")
 const chatRouter = require("./routes/chat")
 const messageRouter = require("./routes/message")
 const groupRouter = require("./routes/group")
+const cloudinary = require('./cloudinary/cloudinary.js')
 const Server = server.createServer(app)
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
